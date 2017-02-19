8076839a71507f185807ac8f383b0eb8f384d157
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-19 19:07:17 +0100

    Added Activity entity

---
+++ ActivityFeed/Domain/Entities/Activity.swift
@@ -0,0 +1,18 @@
+//
+//  Activity.swift
+//  ActivityFeed
+//
+//  Created by Jeremi Kaczmarczyk on 19/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import Foundation
+
+struct Activity {
+    
+    let id: String
+    let message: String
+    let amount: Float
+    let userId: Int
+    let timestamp: Date
+}
