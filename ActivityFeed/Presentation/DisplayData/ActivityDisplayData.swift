3b4f2cc0174712b5386bfb7ab81eabc17dbb4050
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-20 18:13:38 +0100

    Added simple ActivityDisplayData struct

--- ActivityFeed/Presentation/DisplayData/ActivityDisplayData.swift
+++ ActivityFeed/Presentation/DisplayData/ActivityDisplayData.swift
@@ -11,6 +11,23 @@ import Foundation
 struct ActivityDisplayData {
     
     private let activity: Activity
-    private let boldRegex = "<\\s*strong[^>]*>(.*?)<\\s*\\/\\s*strong>"
+    
+    var message: String {
+        return activity.message
+    }
+    
+    var amount: String {
+        // TODO: - Use NumberFormatter
+        return "$\(activity.amount)"
+    }
+    
+    var dateString: String {
+        // TODO: - Inject Locale, Format output, use actual date
+        return "22 Sep 2016"
+    }
+    
+    var imageUrl: URL? {
+        return nil
+    }
     
 }
