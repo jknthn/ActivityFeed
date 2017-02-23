d273f744b59c25f0fbe757a264031eae5fcf16ce
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-23 17:38:47 +0100

    Added ColorPalette class

---
+++ ActivityFeed/UI/Aux/ColorPalette.swift
@@ -0,0 +1,29 @@
+//
+//  ColorPalette.swift
+//  ActivityFeed
+//
+//  Created by Jeremi Kaczmarczyk on 23/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    convenience init(r: UInt8, g: UInt8, b: UInt8, a: Float = 1.0) {
+        self.init(
+            red: CGFloat(r) / 255.0,
+            green: CGFloat(g) / 255.0,
+            blue: CGFloat(b) / 255.0,
+            alpha: CGFloat(a)
+        )
+    }
+}
+
+class ColorPalette {
+    
+    static let background = UIColor.white
+    static let textHighlighted = UIColor.black
+    static let textNormal = UIColor(r: 173, g: 184, b: 189)
+    static let textAmountPositive = UIColor(r: 88, g: 191, b: 10)
+}
