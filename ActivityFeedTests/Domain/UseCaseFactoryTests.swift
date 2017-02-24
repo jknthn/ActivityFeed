f29aa46f0fbd1223657901850d57a7c6f30ed4df
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-24 19:57:24 +0100

    Added UseCaseFactory tests

---
+++ ActivityFeedTests/Domain/UseCaseFactoryTests.swift
@@ -0,0 +1,45 @@
+//
+//  UseCaseFactoryTests.swift
+//  ActivityFeed
+//
+//  Created by Jeremi Kaczmarczyk on 24/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import XCTest
+@testable import ActivityFeed
+
+class UseCaseFactoryTest: XCTestCase {
+    
+    var useCaseFactory: UseCaseFactory!
+    var entityGateway: EnitityGatewayMock!
+    
+    // MARK: - XCTestCase
+    
+    override func setUp() {
+        super.setUp()
+        entityGateway = EnitityGatewayMock()
+        useCaseFactory = UseCaseFactory(entityGateway: entityGateway)
+    }
+    
+    override func tearDown() {
+        entityGateway = nil
+        useCaseFactory = nil
+        super.tearDown()
+    }
+    
+    // MARK: - Tests
+    
+    func testFactoryCreatesShowActivitiesUseCase() {
+        let useCase = useCaseFactory.createUseCase(for: .showActivities(completion: { _ in }))
+        XCTAssertNotNil(useCase as? ShowActivitiesUseCase)
+    }
+    
+    // MARK: - Mocks
+    
+    class EnitityGatewayMock: EntityGateway {
+        
+        func getActivities(for range: Range<Date>, completion: @escaping (Date, [Activity]) -> Void) { }
+        
+    }
+}
