1b02add833a5aa991b73e26148a6ee07bc84322f
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-26 18:37:27 +0100

    Added ActivitiesResponse struct

---
+++ ActivityFeed/Domain/Entities/ActivitiesResponse.swift
@@ -0,0 +1,29 @@
+//
+//  ActivitiesResponse.swift
+//  ActivityFeed
+//
+//  Created by Jeremi Kaczmarczyk on 26/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import Foundation
+
+struct ActivitiesResponse: JsonParsable {
+    
+    let oldest: Date
+    let activities: [Activity]
+    
+    // MARK: - JsonParsable
+    
+    static func fromJson(json: JsonDictionary) -> ActivitiesResponse? {
+        let formatter = ISO8601DateFormatter()
+        guard
+            let oldestRaw = json["oldest"] as? String,
+            let oldest = formatter.date(from: oldestRaw),
+            let activitiesJson = json["activities"] as? [JsonDictionary]
+        else {
+            return nil
+        }
+        return ActivitiesResponse(oldest: oldest, activities: activitiesJson.flatMap(Activity.fromJson))
+    }
+}
