6608149dfed1d0e6c8fa3643b701744293988c75
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-26 18:45:28 +0100

    Added generic get method in ApiRepository class

--- ActivityFeed/Data/ApiRepository.swift
+++ ActivityFeed/Data/ApiRepository.swift
@@ -19,11 +19,35 @@ class ApiRepository: EntityGateway {
     // MARK: - EntityGateway
     
     func getActivities(for range: Range<Date>, completion: @escaping (Result<ActivitiesResponse, Error>) -> Void) {
-        
+        get(.activities(range: range)) { result in
+            completion(result)
+        }
     }
     
     // MARK: - Utility
     
+    private func get<T: JsonParsable>(_ endpoint: ApiEndpoint, completion: @escaping (Result<T, Error>) -> Void) {
+        URLSession.shared.dataTask(with: endpoint.url) { data, response, error in
+            if let _ = error {
+                completion(.error(ApiError.network))
+            }
+            DispatchQueue.main.async {
+                do {
+                    guard
+                        let data = data,
+                        let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? JsonDictionary,
+                        let object = T.fromJson(json: json)
+                    else {
+                            completion(.error(ApiError.parse))
+                            return
+                    }
+                    completion(.success(object))
+                } catch {
+                    completion(.error(ApiError.parse))
+                }
+            }
+        }.resume()
+    }
     
 }
 
