a29c3ac3e41ed39ef5a90987e0bf73f6a55c01ee
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-26 17:27:31 +0100

    Added Result enum

---
+++ ActivityFeed/Domain/Aux/Result.swift
@@ -0,0 +1,26 @@
+//
+//  Result.swift
+//  ActivityFeed
+//
+//  Created by Jeremi Kaczmarczyk on 26/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import Foundation
+
+enum Result<Value, Error> {
+    case success(Value)
+    case error(Error)
+    
+    func onSuccess(_ closure: (Value) -> Void) {
+        if case .success(let value) = self {
+            closure(value)
+        }
+    }
+    
+    func onError(_ closure: (Error) -> Void) {
+        if case .error(let error) = self {
+            closure(error)
+        }
+    }
+}
