7d0d52d1f22f73df49ab55807247bae262ae55a1
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2017-02-26 12:39:07 +0100

    Extracted date range creation logic to DateRangeIterator struct

---
+++ ActivityFeed/Domain/Aux/DateRangeGenerator.swift
@@ -0,0 +1,47 @@
+//
+//  DateRangeGenerator.swift
+//  ActivityFeed
+//
+//  Created by Jeremi Kaczmarczyk on 26/02/2017.
+//  Copyright © 2017 Jeremi Kaczmarczyk. All rights reserved.
+//
+
+import Foundation
+
+struct DateRangeIterator: Sequence, IteratorProtocol {
+    
+    private(set) var count: Int?
+    
+    private var oldestDate = Date(timeIntervalSince1970: 0.0)
+    private var startDate: Date
+    private var endDate: Date
+    
+    // MARK: - Initialization
+    
+    init() {
+        startDate = Date()
+        endDate = startDate.substracting2Weeks()!
+    }
+    
+    // MARK: - Public 
+    
+    mutating func changeOldestDate(to date: Date) {
+        if oldestDate != date {
+            oldestDate = date
+        }
+    }
+    
+    // MARK: - IteratorProtocol
+    
+    mutating func next() -> Range<Date>? {
+        guard startDate > oldestDate else {
+            return nil
+        }
+        
+        let range = Range(uncheckedBounds: (lower: endDate, upper: startDate))
+        startDate = endDate
+        endDate = endDate.substracting2Weeks()!
+        return range
+    }
+    
+}
--- ActivityFeed/Domain/UseCases/ShowActivitiesUseCase.swift
+++ ActivityFeed/Domain/UseCases/ShowActivitiesUseCase.swift
@@ -14,53 +14,25 @@ class ShowActivitiesUseCase: UseCase {
     
     private let entityGateway: EntityGateway
     private let completion: ShowActivitiesCompletion
-    
-    private var oldestDate: Date?
-    
-    private var startDate: Date?
-    private var endDate: Date?
+    private var rangeIterator = DateRangeIterator()
     
     // MARK: - Initialization
     
     init(entityGateway: EntityGateway, completion: @escaping ShowActivitiesCompletion) {
         self.entityGateway = entityGateway
         self.completion = completion
-        
-        startDate = Date()
-        // TODO: - Inject locale
-        endDate = startDate?.substracting2Weeks()
     }
     
     // MARK: - UseCase
     
     func execute() {
-        guard let range = createNextRange() else {
+        guard let range = rangeIterator.next() else {
             return
         }
         entityGateway.getActivities(for: range) { [weak self] oldest, activities in
-            self?.oldestDate = oldest
+            self?.rangeIterator.changeOldestDate(to: oldest)
             self?.completion(activities.map(ActivityDisplayData.init).sorted { $0.0.timestamp > $0.1.timestamp })
         }
     }
-    
-    // MARK: - Private
-    
-    private func createNextRange() -> Range<Date>? {
-        guard let start = startDate, let end = endDate else {
-            // TODO: - Think about it / handle
-            assertionFailure("Dates not set")
-            return nil
-        }
-        
-        if let oldest = oldestDate, start < oldest {
-            return nil
-        }
-        
-        let range = Range(uncheckedBounds: (lower: end, upper: start))
-        startDate = end
-        endDate = end.substracting2Weeks()
-        return range
-    }
-    
 
 }
